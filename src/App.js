26d8289b9647dee30c7c3a2bdc1768fd118d0335
Chance Roberts <70963046+chance-roberts@users.noreply.github.com>
2024-05-08 20:49:51 -0500

    ScreenSize adjustments

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
+import useScreenSize from './useScreenSize';
 
 export default function App() {
+  const screenSize = useScreenSize();
+
   return(
-    <main className = "text-gray-400 bg-gray-0=900 body-font">
+    <main className = "text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
       <Skills />
       <Education />
       <Contact />
+      <div>
+        <h1>Screen Size Detection with React Hook</h1>
+        <p>Width: {screenSize.width}</p>
+        <p>Height: {screenSize.height}</p>
+      </div>
     </main>
   );
 }
